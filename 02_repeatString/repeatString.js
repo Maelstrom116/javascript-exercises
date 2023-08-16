80725a1b894efd536f639db8731faa87c096c9bd
Trevor Hilz <trevorhilz@gmail.com>
2023-08-16 09:10:59 -0500

    Completed problems 01, 02, 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    let greeting = '';
+    for(let i = 0; i < times; i++) {
+        greeting += str
+    }
+    
+    if (times < 0) {
+        return 'ERROR'
+    } else {
+        return greeting;
+    }
+}
 
-};
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split('');
+    let revArray = splitString.reverse();
+    let joinArray = revArray.join('');
+    return joinArray;
 };
 
 // Do not edit below this line
