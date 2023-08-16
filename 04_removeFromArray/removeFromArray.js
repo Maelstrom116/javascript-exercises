6ed54face3937e46e9575fed364ff1d490f75906
Trevor Hilz <trevorhilz@gmail.com>
2023-08-16 09:11:43 -0500

    In process, problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(num, remove) {
+    let cars = [num];
+    let arrayless = cars.from(remove);
+    return arrayless;
 };
 
 // Do not edit below this line
