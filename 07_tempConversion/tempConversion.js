442677b298a56b63c4b0539301ee1583849099d4
Trevor Hilz <trevorhilz@gmail.com>
2023-08-17 09:44:34 -0500

    Completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  return Math.round(10 * ((fahr - 32) * (5 / 9))) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  return Math.round(10 * ((cel * (9 / 5) + 32))) / 10;
 };
 
 // Do not edit below this line
